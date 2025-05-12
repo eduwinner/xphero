0c1fbf682c9f102d16ff06d1a2bf276a80efdb8c
eduwinner <eduardo@winnertecnologia.com.br>
2025-05-12 20:30:35 -0300

    primeiro commit

--- index.js
+++ index.js
@@ -2,41 +2,58 @@
 let nomeHero
 let totalXP
 let nivelAtual
-//inicialização das variáveis
-nomeHero = prompt("Qual é o nome do seu herói?")
-if (nomeHero !== null) {
-  console.log("Olá, " + nomeHero + "!!!")
-} else {
-  console.log("O nome do herói precisa ser informado!!!")
+//necessita prompt-sync para inerarir como usuário
+const prompt = require('prompt-sync')();
+//Incicialização das Variáveis
+nomeHero=""
+while (nomeHero==""){
+    nomeHero = prompt("Qual é o nome do seu herói? ")
+    if (nomeHero !== "") {
+        console.log("Olá, " + nomeHero + "!!!")
+        } else {
+            console.log("O nome do herói precisa ser informado!!!")
+            }
 }
-totalXP = prompt("Informe a quntidade de pontos XP do seu herói !!!")
-if (totalXP !== null) {
-  console.log("Seu herói tem " + totalXP + "pontos XP !!! Vamos descobrir seu nível atual")
-} else {
-  console.log("A quantidade de pontos XP precisa ser informada!!!")
+totalXP=0
+while (totalXP==0){
+    totalXP = prompt("Informe a quantidade de pontos XP do seu herói: ")
+    if (totalXP > 0) {
+        console.log("Seu herói tem " + totalXP + " pontos XP !!! Vamos descobrir o nível atual, aguarde...")
+        }else {
+            console.log("A quantidade de pontos XP precisa ser informada e deve ser maior que zero!!!")
+            }
 }
 nivelAtual=""
-switch (totalXP){
-    case totalXP <=2000:
+switch (true){
+    case totalXP<=2000 :
         nivelAtual="Bronze"
         break
-    case totalXP <=5000:
+    case totalXP<=5000 :
         nivelAtual="Prata"
         break
-    case totalXP <=7000:
+    case totalXP<=7000 :
         nivelAtual="Ouro"
         break
-    case totalXP <=8000:
+    case totalXP<=8000 :
         nivelAtual="Platina"
         break
-    case totalXP <=9000:
+    case totalXP<=9000 :
         nivelAtual="Ascendente"
         break
-    case totalXP <=10000:
+    case totalXP<=10000 :
         nivelAtual="Imortal"
         break
-    case totalXP >=10001:
+    case totalXP>=10001 :
         nivelAtual="Radiante"
         break
 }
-console.log("O herói de nome "+nomeHero + "está no nível de " + nivelAtual)
+// Vamos aguardar 5 segundos para criar um suspense
+  setTimeout(() => {
+  console.log('Calculando seu nível...Aguarde...');
+}, 5000)
+ setTimeout(() => {
+  console.log('Aguarde só mais um instante...');
+}, 8000)
+ setTimeout(() => {
+  console.log("O herói de nome "+nomeHero + " está no nível " + nivelAtual)
+}, 10000)
