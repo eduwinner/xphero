9e141d023c7b918f9da3d1e3858277d4f3cabf82
eduwinner <eduardo@winnertecnologia.com.br>
2025-05-12 21:01:16 -0300

    versão final

--- index.js
+++ index.js
@@ -8,20 +8,34 @@ const prompt = require('prompt-sync')();
 nomeHero=""
 while (nomeHero==""){
     nomeHero = prompt("Qual é o nome do seu herói? ")
+    console.log()
     if (nomeHero !== "") {
         console.log("Olá, " + nomeHero + "!!!")
+        console.log()
         } else {
             console.log("O nome do herói precisa ser informado!!!")
+            console.log()
             }
 }
 totalXP=0
 while (totalXP==0){
     totalXP = prompt("Informe a quantidade de pontos XP do seu herói: ")
-    if (totalXP > 0) {
+    console.log()
+    if (!isNaN(totalXP)) {
+        if (totalXP > 0) {
         console.log("Seu herói tem " + totalXP + " pontos XP !!! Vamos descobrir o nível atual, aguarde...")
+        console.log()
         }else {
             console.log("A quantidade de pontos XP precisa ser informada e deve ser maior que zero!!!")
+            console.log()
             }
+        } else {
+            totalXP=0
+            console.log("Você não digitou um número válido")
+            console.log()
+            console.log("A quantidade de pontos XP precisa ser informada e deve ser maior que zero!!!")
+            console.log()
+        }
 }
 nivelAtual=""
 switch (true){
@@ -50,11 +64,15 @@ switch (true){
 // Descobrindo o nível do herói. Vamos aguardar 10 segundos para criar um suspense
 // Vamos aguardar 10 segundos para criar um suspense
   setTimeout(() => {
-  console.log('Calculando seu nível...Aguarde...');
+  console.log('Calculando o nível do herói... Aguarde...')
+  console.log()
 }, 4000)
  setTimeout(() => {
-  console.log('Aguarde só mais um instante...');
+  console.log('Aguarde só mais um instante...')
+  console.log()
+  console.log()
 }, 8000)
  setTimeout(() => {
   console.log("O herói de nome "+nomeHero + " está no nível " + nivelAtual)
+  console.log()
 }, 10000)
